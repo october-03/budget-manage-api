520c9c9502d6e4b23292c18a98711cc4aebd99ce
october-03 <season_autumn@kakao.com>
2024-04-22 13:17:58 +0900

    PSQL Event Trigger 수정

--- postgres/init.sql
+++ postgres/init.sql
@@ -8,42 +8,64 @@ CREATE TABLE bank_transaction_logs (
 
 );
 
-CREATE OR REPLACE FUNCTION send_to_es(json_data text, index_name text)
+CREATE OR REPLACE FUNCTION send_to_es(action text, json_data text, index_name text)
 RETURNS void AS $$
 import requests
 import json
 
-def send_to_es(data, index):
-    es_url = f"http://elasticsearch:9200/{index}/_doc"
+def send_to_es(action, data, index):
+    payload = json.loads(data)
+    doc_id = payload['id']
+
+    es_url = f"http://elasticsearch:9200/{index}/_doc/{doc_id}" if doc_id else f"http://elasticsearch:9200/{index}/_doc"
     headers = {
         "Content-Type": "application/json"
     }
 
-    payload = json.loads(data)
+    if action == 'index':
+        response = requests.post(es_url, headers=headers, json=payload)
+    elif action == 'delete':
+        response = requests.delete(es_url, headers=headers)
 
-    response = requests.post(es_url, headers=headers, json=payload)
+    if response.status_code not in [200, 201]:
+        raise Exception(f"Failed to {action} data to ES: {response.text}")
 
-    if response.status_code != 201:
-        raise Exception(f"Failed to send data to ES: {response.text}")
-
-send_to_es(json_data, index_name)
+send_to_es(action, json_data, index_name)
 $$ LANGUAGE plpython3u;
 
 CREATE OR REPLACE FUNCTION sync_to_es_card()
 RETURNS TRIGGER AS $$
 DECLARE
   json_data text;
+  action text;
 BEGIN
+    CASE
+        WHEN TG_OP = 'INSERT' THEN
+            action := 'index';
+        WHEN TG_OP = 'UPDATE' THEN
+            action := 'index';
+        WHEN TG_OP = 'DELETE' THEN
+            action := 'delete';
+        ELSE
+            RAISE EXCEPTION 'Unknown operation: %', TG_OP;
+    END CASE;
+
     RAISE NOTICE 'Data sent to ES';
-    json_data = json_build_object(
-        'id', NEW.id,
-        'payment_type', NEW.payment_type,
-        'amount', NEW.amount,
-        'transaction_date', NEW.transaction_date,
-        'description', NEW.description,
-        'card_id', NEW.card_id
-    )::text;
-    PERFORM send_to_es(json_data, 'card_transaction_logs_index');
+
+    IF TG_OP != 'DELETE' THEN
+        json_data = json_build_object(
+            'id', NEW.id::text,
+            'payment_type', NEW.payment_type,
+            'amount', NEW.amount,
+            'transaction_date', NEW.transaction_date,
+            'description', NEW.description,
+            'card_id', NEW.card_id
+        )::text;
+    ELSE
+        json_data = json_build_object('id', OLD.id::text)::text;
+    END IF;
+
+    PERFORM send_to_es(action, json_data, 'card_transaction_logs_index');
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
@@ -57,17 +79,35 @@ CREATE OR REPLACE FUNCTION sync_to_es_bank()
 RETURNS TRIGGER AS $$
 DECLARE
   json_data text;
+  action text;
 BEGIN
+    CASE
+        WHEN TG_OP = 'INSERT' THEN
+            action := 'index';
+        WHEN TG_OP = 'UPDATE' THEN
+            action := 'index';
+        WHEN TG_OP = 'DELETE' THEN
+            action := 'delete';
+        ELSE
+            RAISE EXCEPTION 'Unknown operation: %', TG_OP;
+    END CASE;
+
     RAISE NOTICE 'Data sent to ES';
-    json_data = json_build_object(
-        'id', NEW.id,
-        'transaction_type', NEW.transaction_type,
-        'amount', NEW.amount,
-        'transaction_date', NEW.transaction_date,
-        'description', NEW.description,
-        'account_id', NEW.account_id
-    )::text;
-    PERFORM send_to_es(json_data, 'bank_transaction_logs_index');
+
+    IF TG_OP != 'DELETE' THEN
+        json_data = json_build_object(
+            'id', NEW.id,
+            'transaction_type', NEW.transaction_type,
+            'amount', NEW.amount,
+            'transaction_date', NEW.transaction_date,
+            'description', NEW.description,
+            'account_id', NEW.account_id
+        )::text;
+    ELSE
+        json_data = json_build_object('id', OLD.id)::text;
+    END IF;
+
+    PERFORM send_to_es(action, json_data, 'bank_transaction_logs_index');
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
